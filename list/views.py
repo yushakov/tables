75fa9d3813aaae84aa38af3ec19e35b98cb7cd81
Yury Ushakov <nomah@list.ru>
2023-03-23 22:31:50 +0000

    Corrected progress (according to money). Added new task field inputs to extend a project.

--- list/views.py
+++ list/views.py
@@ -16,20 +16,20 @@ def detail(request, construct_id):
     ch_list = []
     construct_total_price = 0.0
     construct_progress = 0.0
-    total_working_days = 0
     for choice in construct.choice_set.all():
-        total_working_days += choice.plan_days_num
-        construct_progress += choice.plan_days_num * 0.01 * choice.progress_percent_num
         choice_price = choice.price_num * choice.quantity_num
+        construct_progress += choice_price * 0.01 * choice.progress_percent_num
         construct_total_price += choice_price
         ch_list.append({'choice': choice, 'choice_total_price': choice_price})
-    construct_progress *= 100. / total_working_days
-    print(f'total days: {total_working_days}')
+    construct_progress *= 100. / construct_total_price 
     construct.overall_progress_percent_num = construct_progress
     construct.save()
+    form_tags = [{'show0':f'show{i}', 'show1':f'show{i+1}', 'f0':f'f{i}', 'display':'block' if i == 0 else 'none'}
+                for i in range(5)]
     context = {'construct': construct,
                'ch_list': ch_list,
                'construct_total': construct_total_price,
-               'construct_total_vat': construct_total_price * (1. + 0.01*construct.vat_percent_num)}
+               'construct_total_vat': construct_total_price * (1. + 0.01*construct.vat_percent_num),
+               'form_tags': form_tags}
     return render(request, 'list/detail.html', context)
 
