a4312d622aaf8f0f994b615bddac2a8e52aeeb83
Yury Ushakov <nomah@list.ru>
2023-04-26 23:39:49 +0100

    Table adjustments.

--- list/static/list/detail.js
+++ list/static/list/detail.js
@@ -78,14 +78,14 @@ function showPrettyRaw(x) {
         rows.forEach(function(row) {
            row.cells[g_action_cell_idx].innerHTML = getActionCellHtml(row.rowIndex);
            if(row.rowIndex == 0) {
-               row.cells[g_del_cell_idx].style.display = 'block';
+               row.cells[g_del_cell_idx].style.display = 'table-cell';
                return;
            }
            if(row.cells[g_name_cell_idx].classList.contains('delete')) {
                row.style.display = 'table-row';
            }
            if(row.classList.contains('Choice')) {
-               row.cells[g_del_cell_idx].style.display    = 'block';
+               row.cells[g_del_cell_idx].style.display    = 'table-cell';
            } else {
                var header_del = row.cells[g_del_cell_idx-g_header_del_col_span+1];
                header_del.style.display = 'block';
