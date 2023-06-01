f8499254267f7224799091e875385ca32e1290b3
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-06-01 10:30:42 +0000

    Redirect to admin page from the root url.

--- constructivechoice_dev/urls.py
+++ constructivechoice_dev/urls.py
@@ -17,8 +17,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def redirect_to_admin(request):
+    return redirect('admin:index')
+
 
 urlpatterns = [
+    path('', redirect_to_admin),
     path('list/', include('list.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
