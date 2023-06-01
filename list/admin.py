b13cbd04388c51858a55c7eeddb38131360adc50
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-06-01 22:57:33 +0000

    Displaying invoice status in the list of invoices.

--- list/admin.py
+++ list/admin.py
@@ -8,7 +8,7 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ["get_from_txt", "get_to_txt", "number_link", "within", "transaction_type", "date", "amount"]
 
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ["seller", "within", "number_link", "invoice_type", "issue_date", "due_date", "amount"]
+    list_display = ["seller", "within", "number_link", "invoice_type", "status", "issue_date", "due_date", "amount"]
 
 admin.site.register(Construct, ConstructAdmin)
 admin.site.register(Worker)
