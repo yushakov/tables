83f8f29ea7f7df58010332962b2404bf9771b31f
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-06-02 00:21:02 +0000

    Redirects to all invoices and all transactions from the corresponding URLs.

--- list/urls.py
+++ list/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
 from . import views
+from django.shortcuts import redirect
+
+
+def redirect_to_admin_transactions(request):
+    return redirect('admin:list_transaction_changelist')
+
+def redirect_to_admin_invoices(request):
+    return redirect('admin:list_invoice_changelist')
 
 app_name = 'list'
 urlpatterns = [
@@ -8,7 +16,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:construct_id>/', views.detail, name='detail'),
     path('<int:construct_id>/gantt/', views.gantt, name='gantt'),
+    path('invoice/', redirect_to_admin_invoices),
     path('invoice/<int:invoice_id>/', views.view_invoice, name='view_invoice'),
+    path('transaction/', redirect_to_admin_transactions),
     path('transaction/<int:transaction_id>/', views.view_transaction, name='view_transaction'),
     path('transaction/submit/', views.submit_transaction, name='submit_transaction')
 ]
