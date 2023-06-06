3fd2b3a47497fb4911a214c414ee971d13f32bcc
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-06-06 14:31:05 +0000

    Add favicon.ico to the admin panel.

--- constructivechoice_dev/settings.py
+++ constructivechoice_dev/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import netifaces
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,7 +72,7 @@ ROOT_URLCONF = 'constructivechoice_dev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
