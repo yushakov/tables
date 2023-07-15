b00164c66c79904038d4c7e0811f7fe2d803d601
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-07-15 12:35:31 +0000

    Shorten log messages.

--- constructivechoice_dev/settings.py
+++ constructivechoice_dev/settings.py
@@ -150,7 +150,7 @@ LOGGING = {
     "disable_existing_loggers": False,
     'formatters': {
         'verbose': {
-            'format': '{levelname} {asctime} {module} {process:d} {thread:d} {message}',
+            'format': '{levelname} {asctime} {module} {message}',
             'style': '{',
         },
         'simple': {
