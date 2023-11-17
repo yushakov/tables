d49e633badf53748835652600349de28babe5aa8
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-11-17 14:51:23 +0000

    Calculate VAT from the actual income.

--- list/views.py
+++ list/views.py
@@ -36,6 +36,7 @@ def get_total(constructs):
     total = {}
     full_cost = 0
     full_cost_vat = 0
+    vat_from_income = 0
     full_progress_cost = 0
     round_income = 0
     round_outcome = 0
@@ -50,6 +51,7 @@ def get_total(constructs):
     for con in constructs:
         full_cost += con.full_cost
         full_cost_vat += con.full_cost_vat
+        vat_from_income += con.vat_from_income
         full_progress_cost += con.full_progress_cost
         round_income += con.round_income
         round_outcome += con.round_outcome
@@ -73,6 +75,7 @@ def get_total(constructs):
     total['round_outcome'] = round_outcome
     total['full_cost'] = full_cost
     total['full_cost_vat'] = full_cost_vat
+    total['vat_from_income'] = vat_from_income
     total['full_progress_cost'] = full_progress_cost
     total['overall_progress_percent_num'] = 0.0
     if full_cost > 1.e-4:
