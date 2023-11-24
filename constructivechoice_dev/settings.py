4fe6347e8844089582352776f56c85d635b7db55
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-11-24 15:30:06 +0000

    Set longer task names for Gantt. Add stuff for local debugging (CORS, etc.)

--- constructivechoice_dev/settings.py
+++ constructivechoice_dev/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'list.apps.ListConfig',
     'gantt.apps.GanttConfig',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,11 +68,24 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000"
+]
+CORS_ALLOW_METHODS = [
+    "GET"
+]
+CORS_ALLOW_HEADERS = [
+    "content-type"
 ]
 
 ROOT_URLCONF = 'constructivechoice_dev.urls'
