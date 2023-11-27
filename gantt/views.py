2d7370760dd412f22e4f40b2d9511b7e7ccbb0d4
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-11-27 18:30:01 +0000

    Distribute tasks under sub-headers in the Gantt chart.

--- gantt/views.py
+++ gantt/views.py
@@ -5,11 +5,16 @@ from .serializers import TaskSerializer
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.utils import timezone
+import json
+from copy import deepcopy as copy
 
 class ChoiceViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TaskSerializer
 
     def get_queryset(self):
+        # TODO: refactor carefully
+        # TODO: write tests
+        # TODO: fix the BUG: two consequtive headers on the top break the chart
         queryset = []
         if not self.request.user.is_authenticated:
             get_object_or_404(Construct, pk=-1)
@@ -17,18 +22,41 @@ class ChoiceViewSet(viewsets.ReadOnlyModelViewSet):
         if construct_id >= 0:
             construct = Construct.objects.get(pk=construct_id)
             choices = Choice.objects.filter(construct__id=construct_id).order_by('plan_start_date')
-            queryset = [TaskSerializer.transform_choice_to_task(choice) for choice in choices]
-            project = {'id': construct.title_text,
+            project_struct = json.loads(construct.struct_json)
+            group_id = construct.title_text
+            project = {'id': group_id,
                     'construct_name': '',
-                    'name_txt': construct.title_text,
+                    'name_txt': group_id,
                     'plan_start_date': timezone.now().date(),
                     'plan_days_num': 1,
                     'type': 'project',
                     'progress_percent_num': 0,
                     'hide_children': 0,
-                    'display_order': 1}
-            queryset = [project] + queryset
-        return queryset
+                    'display_order': 0}
+            queryset.append(copy(project))
+            dates = []
+            tmp_set = []
+            for key, val in project_struct.items():
+                if val['type'].find('Choice') >= 0:
+                    choice = choices.filter(id=val['id'])[0]
+                    task = TaskSerializer.transform_choice_to_task(choice)
+                    task['construct_name'] = group_id
+                    task['display_order'] = int(key.replace('line_', ''))
+                    tmp_set.append(task)
+                    dates.append([task['plan_start_date'], task['plan_days_num']])
+                else:
+                    queryset += sorted(tmp_set, key=lambda x: x['plan_start_date'])
+                    tmp_set = []
+                    group_id = val['id']
+                    project['id'] = project['name_txt'] = group_id
+                    project['display_order'] = int(key.replace('line_', ''))
+                    queryset.append(copy(project))
+            queryset += sorted(tmp_set, key=lambda x: x['plan_start_date'])
+            out = []
+            for i, q in enumerate(queryset):
+                q['display_order'] = i + 1
+                out.append(q)
+        return out
 
 
 @login_required
