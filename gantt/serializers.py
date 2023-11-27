7e59551fe59fba001eb3682b8eecbae72d493bc7
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2023-11-27 15:10:22 +0000

    Change ChoiceSerializer to TaskSerializer. For the same Gantt chart functionality.

--- gantt/serializers.py
+++ gantt/serializers.py
@@ -1,8 +1,35 @@
 from rest_framework import serializers
 from list.models import Choice 
 
-class ChoiceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Choice
-        fields = ['id', 'construct_name', 'name_txt', 'plan_start_date', 'plan_days_num', 'progress_percent_num']
+class TaskSerializer(serializers.Serializer):
+    id = serializers.CharField(read_only=True, max_length=500)
+    construct_name = serializers.CharField(required=False, allow_blank=True, max_length=500)
+    name_txt = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    plan_start_date = serializers.DateField()
+    plan_days_num = serializers.IntegerField()
+    progress_percent_num = serializers.IntegerField(read_only=True)
+    type = serializers.CharField(required=False, max_length=100)
+    hide_children = serializers.IntegerField(read_only=True)
+    display_order = serializers.IntegerField(read_only=True)
 
+    def create(self, validated_data):
+        # Simply return the validated data as a new "Task"
+        return validated_data
+
+    def update(self, instance, validated_data):
+        # Update the instance dictionary with validated data
+        for attr, value in validated_data.items():
+            instance[attr] = value
+        return instance
+
+    @staticmethod
+    def transform_choice_to_task(choice):
+        return {
+            'id': str(choice.id),
+            'construct_name': choice.construct.title_text,
+            'name_txt': choice.name_txt,
+            'plan_start_date': choice.plan_start_date,
+            'plan_days_num': choice.plan_days_num,
+            'progress_percent_num': choice.progress_percent_num,
+            'type': 'task'
+        }
--- gantt/views.py
+++ gantt/views.py
@@ -1,22 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from list.models import Choice, Construct
-from .serializers import ChoiceSerializer
+from .serializers import TaskSerializer
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.utils import timezone
 
 class ChoiceViewSet(viewsets.ReadOnlyModelViewSet):
-    serializer_class = ChoiceSerializer
+    serializer_class = TaskSerializer
 
     def get_queryset(self):
-        queryset = Choice.objects.all()[:1]
+        queryset = []
+        if not self.request.user.is_authenticated:
+            get_object_or_404(Construct, pk=-1)
         construct_id = int(self.request.GET.get('id', '-1'))
         if construct_id >= 0:
-            queryset = Choice.objects.filter(construct__id=construct_id).order_by('plan_start_date')
+            construct = Construct.objects.get(pk=construct_id)
+            choices = Choice.objects.filter(construct__id=construct_id).order_by('plan_start_date')
+            queryset = [TaskSerializer.transform_choice_to_task(choice) for choice in choices]
+            project = {'id': construct.title_text,
+                    'construct_name': '',
+                    'name_txt': construct.title_text,
+                    'plan_start_date': timezone.now().date(),
+                    'plan_days_num': 1,
+                    'type': 'project',
+                    'progress_percent_num': 0,
+                    'hide_children': 0,
+                    'display_order': 1}
+            queryset = [project] + queryset
         return queryset
 
-
-# Create your views here.
 
 @login_required
 def index(request, construct_id):
