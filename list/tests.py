7d899006b1008ca3d1db3bec120f7bc4b951685f
Yury Ushakov <yuriy.v.ushakov@gmail.com>
2024-04-24 21:07:54 +0100

    Test negative prices in the invoice printing functionality.

--- list/tests.py
+++ list/tests.py
@@ -2532,7 +2532,6 @@ class ViewTests(TestCase):
         out = get_number(line)
         self.assertEqual(out, 12000.0)
         line = 'a12,000'
-        # import pdb; pdb.set_trace()
         out = get_number(line)
         self.assertEqual(out, 12000.0)
         line = '  12,000'
@@ -2550,6 +2549,9 @@ class ViewTests(TestCase):
         line = ' ab , klsje  dl'
         out = get_number(line)
         self.assertEqual(out, 0.0)
+        line = ' -971.11'
+        out = get_number(line)
+        self.assertEqual(out, -971.11)
 
     def test_print_invoice_warning(self):
         c = Client()
@@ -2581,6 +2583,25 @@ class ViewTests(TestCase):
                 'status': ['Unpaid'], 'initial-issue_date': ['2023-07-12 07:47:28+00:00'],
                 'initial-due_date': ['2023-07-12 07:47:28+00:00'], 'initial-photo': ['Raw content'],
                 'details_txt': ['1 nr, desr, 20\n 2 hour, descr2, 40'], 'photo': ['']}
+        response = c.post("/list/invoice/submit/", post_data)
+        self.assertIs(response.url.find('accounts/login') >= 0, False)
+        self.assertEqual(response.status_code, STATUS_CODE_REDIRECT)
+        invoice = Invoice.objects.latest()
+        response = c.get(f"/list/invoice/{invoice.id}/print/")
+        self.assertEqual(response.status_code, STATUS_CODE_OK)
+        self.assertTrue(len(response.context['warning']) == 0)
+
+    def test_print_invoice_discount(self):
+        c = Client()
+        c.login(username="yuran", password="secret")
+        cons = Construct()
+        cons.save()
+        post_data = {'seller': ['Vasya'], 'amount': ['70'],
+                'invoice_type': ['IN'], 'construct': [str(cons.id)], 'issue_date': ['2023-05-20'],
+                'due_date': ['2023-05-21'], 'number': ['12345678'], 'initial-number': ['12345678'],
+                'status': ['Unpaid'], 'initial-issue_date': ['2023-07-12 07:47:28+00:00'],
+                'initial-due_date': ['2023-07-12 07:47:28+00:00'], 'initial-photo': ['Raw content'],
+                'details_txt': ['1 nr, descr, 20\n 2 hour, descr2, 40\n 1 item, discount, -30'], 'photo': ['']}
         response = c.post("/list/invoice/submit/", post_data)
         self.assertIs(response.url.find('accounts/login') >= 0, False)
         self.assertEqual(response.status_code, STATUS_CODE_REDIRECT)
